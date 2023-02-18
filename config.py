cfc2de4790fa431f080540a327c75f38a53b3677
bowen <xubowen495@gmail.com>
2023-02-18 18:28:02 +0800

    Update test config

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 
 
 class BaseConfig:
-    MONGODB_HOST = os.environ.get('MONGODB_HOST', 'mongodb://localhost:27017/task2xbw?serverSelectionTimeoutMs=500')
+    MONGODB_HOST = os.environ.get('MONGODB_HOST', 'mongodb://localhost:27017/task2xbw?serverSelectionTimeoutMs=5000')
 
 
 class TestConfig(BaseConfig):
-    MONGODB_HOST = os.environ.get('MONGODB_HOST', 'mongodb://localhost:27017/task2xbw-test?serverSelectionTimeoutMs=500')
-
+    MONGODB_HOST = os.environ.get('MONGODB_HOST',
+                                  'mongodb://localhost:27017/task2xbw-test?serverSelectionTimeoutMs=5000')
